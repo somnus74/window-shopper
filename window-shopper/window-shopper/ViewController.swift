2581c83599dec4d2aff54be6b5ad5609f7a43f2c
Malcolm Edwards <malcolm@Artemis.local>
2018-08-30 11:55:56 +1000

    Project created in xcode

---
+++ window-shopper/window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Malcolm Edwards on 30/8/18.
+//  Copyright © 2018 Xenophile Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
